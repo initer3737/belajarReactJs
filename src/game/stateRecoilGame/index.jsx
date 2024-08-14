d613aec8c2e4d87d9664909d01d6a60ac1aa8a10
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2024-08-14 16:28:52 +0700

    update

--- src/game/stateRecoilGame/index.jsx
+++ src/game/stateRecoilGame/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useCallback,useMemo,useState} from 'react'
 import { useRecoilState,useRecoilValue } from 'recoil'
 import { Data,DataSelector } from './data'
     const useConvertScoreToStatus=(score)=>{
@@ -14,6 +14,15 @@ import { Data,DataSelector } from './data'
 export default function GameState() {
     const [scoreGame,setScoreGame]=useRecoilState(Data)
     const getScore=useRecoilValue(DataSelector)
+    const [score,setScore]=useState(0)
+    const [score2,setScore2]=useState(0)
+    const Analitics=()=>{
+        for (let index = 0; index < 2000000000; index++) {
+            
+        }
+        console.log("anime is good")
+    }
+    const getAnalitycs=useMemo(()=>Analitics(),[score])
   return (
     <div className='flex flex-col justify-center align-items-center h-screen'>
         <p className='text-center text-2xl'>{
@@ -25,6 +34,20 @@ export default function GameState() {
                                   //replace the old data but overide this one
             onClick={()=>setScoreGame({...scoreGame, score:scoreGame.score+1} ) }
         >hit me!</button>
+                <div className="flex flex-col gap-3 justify-center">
+                    <h2 className="test-3xl text-center">game desu score {score}</h2>
+                    <h3 className="text-2xl"><hr />{getAnalitycs}</h3>
+                    <button onClick={()=>{
+                        setScore(prev=>prev+1)
+                    }}>hit me kudasai</button>
+                </div>
+                <div className="flex flex-col gap-3 justify-center">
+                    <h2 className="test-3xl text-center">game desu score2 {score2}</h2>
+                    <h3 className="text-2xl"><hr /></h3>
+                    <button onClick={()=>{
+                        setScore2(prev=>prev+1)
+                    }}>hit me kudasai2</button>
+                </div>
     </div>
   )
 }
